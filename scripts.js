8b34c0516dbc4a1fe59bd097c06bf3709f4e7f94
Korratus2003 <95227592+Korratus2003@users.noreply.github.com>
2024-03-17 13:37:20 +0100

    Add files via upload

---
+++ scripts.js
@@ -0,0 +1,70 @@
+const container = document.querySelector("#container");
+const heart = document.querySelector("#heart");
+let isDragging = false;
+let currentStokrotka = null;
+let offsetX = 0;
+let offsetY = 0;
+
+function getRandomInt(max) {
+  return Math.floor(Math.random() * max) + 1;
+}
+
+function generateStokrotka() {
+  const stokrotka = document.createElement("div");
+  stokrotka.className = "flower";
+  stokrotka.style.backgroundImage = `url("${getRandomInt(3)}.png")`;
+  container.appendChild(stokrotka);
+  stokrotka.style.top = `${getRandomInt(window.innerHeight - stokrotka.offsetWidth)}px`;
+  stokrotka.style.left = `${getRandomInt(window.innerWidth - stokrotka.offsetWidth)}px`;
+
+  stokrotka.addEventListener("mousedown", handleMouseDown);
+  stokrotka.addEventListener("mousemove", handleMouseMove);
+  stokrotka.addEventListener("mouseup", handleMouseUp);
+  stokrotka.addEventListener("touchstart", handleTouchStart);
+  stokrotka.addEventListener("touchmove", handleTouchMove);
+  stokrotka.addEventListener("touchend", handleTouchEnd);
+}
+
+function handleMouseDown(e) {
+  isDragging = true;
+  currentStokrotka = e.target;
+  offsetX = e.clientX - currentStokrotka.getBoundingClientRect().left;
+  offsetY = e.clientY - currentStokrotka.getBoundingClientRect().top;
+  currentStokrotka.style.position = "absolute";
+  currentStokrotka.style.zIndex = "1000";
+}
+
+function handleMouseMove(e) {
+  if (isDragging && currentStokrotka) {
+    currentStokrotka.style.left = `${e.clientX - offsetX}px`;
+    currentStokrotka.style.top = `${e.clientY - offsetY}px`;
+  }
+}
+
+function handleMouseUp() {
+  isDragging = false;
+  currentStokrotka = null;
+}
+
+function handleTouchStart(e) {
+    isDragging = true;
+    currentStokrotka = e.target;
+    const touch = e.touches[0];
+    offsetX = touch.clientX - currentStokrotka.getBoundingClientRect().left;
+    offsetY = touch.clientY - currentStokrotka.getBoundingClientRect().top;
+  }
+  
+  function handleTouchMove(e) {
+    if (isDragging && currentStokrotka) {
+      const touch = e.touches[0];
+      currentStokrotka.style.left = `${touch.clientX - offsetX}px`;
+      currentStokrotka.style.top = `${touch.clientY - offsetY}px`;
+    }
+  }
+  
+  function handleTouchEnd() {
+    isDragging = false;
+    currentStokrotka = null;
+  }
+
+heart.addEventListener("click", generateStokrotka);
